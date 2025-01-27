7922bcfd825d799c9be321d6742bf2e6d5f69678
taurasbear <youngicorn@gmail.com>
2025-01-27 18:03:00 +0200

    Initial commit.

---
+++ server.c
@@ -0,0 +1,13 @@
+#define WIN32_LEAN_AND_MEAN
+#include <stdio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world from server!");
+
+    return 0;
+}
